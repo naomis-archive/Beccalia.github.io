7dc498be6db586923d559df2d7d30cf0bdac7179
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-12-18 09:48:30 -0800

    fix: tests

--- src/app/safeurl.pipe.spec.ts
+++ src/app/safeurl.pipe.spec.ts
@@ -1,8 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+import { DomSanitizer } from '@angular/platform-browser';
 import { SafeurlPipe } from './safeurl.pipe';
 
 describe('SafeurlPipe', () => {
+  let sanitized: DomSanitizer;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [DomSanitizer],
+    });
+    sanitized = TestBed.inject(DomSanitizer);
+  });
   it('create an instance', () => {
-    const pipe = new SafeurlPipe();
+    const pipe = new SafeurlPipe(sanitized);
     expect(pipe).toBeTruthy();
   });
 });
