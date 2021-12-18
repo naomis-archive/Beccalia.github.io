1138d898edfaf59903c61a6a5b9870c0d890a364
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-12-17 21:31:00 -0800

    feat: enable ga

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
+declare let gtag: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'profile';
+  constructor(public router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'G-127PPXGFVS', {
+          'page-path': event.urlAfterRedirects,
+        });
+      }
+    });
+  }
 }
