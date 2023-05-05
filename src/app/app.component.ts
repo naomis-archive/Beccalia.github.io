1af11d693db0d65fc556b0261504abce7adb7af6
Naomi Carrigan <nhcarrigan@gmail.com>
2023-05-05 15:47:59 -0700

    fix: no gtag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-
-declare let gtag: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,13 +6,5 @@ declare let gtag: Function;
 })
 export class AppComponent {
   title = 'profile';
-  constructor(public router: Router) {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        gtag('config', 'G-127PPXGFVS', {
-          'page-path': event.urlAfterRedirects,
-        });
-      }
-    });
-  }
+  constructor() {}
 }
